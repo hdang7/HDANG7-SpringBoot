44acacbd81a1ca4348a1b5fcffa6f02a14ea5f22
hdang7 <hdang7@outlook.com>
2019-08-05 13:08:10 -0700

    Revert "c2"
    This reverts commit 9a00ec6970e98b97a57c78502e4840058ca8535f.

--- src/main/java/com/example/bean/UserBean - Copy.java
+++ src/main/java/com/example/bean/UserBean - Copy.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
-import com.example.role.Role; bbb
+import com.example.role.Role; aaa
 
 /**
  *
